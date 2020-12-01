186fb842e4331a6eaeaf796f5ec1a6c1d40ef863
juliocjs <60446744+juliocjs@users.noreply.github.com>
2020-12-01 13:04:18 -0800

    started a website

---
+++ assets/styles/rideit.js
@@ -0,0 +1,5 @@
+new TypeIt("#ride", {
+    strings: "1. Wildcat Trail (Click the image for step-by-step route!)",
+    speed: 75,
+    loop: false
+  }).go();
---
+++ assets/styles/typeit.js
@@ -0,0 +1,8 @@
+new TypeIt("#introduction", {
+    strings: "Hello! My name is Julio Jaramillo and I am a student/cyclist in the Bay Area. Stick around for some bike checks and some local routes. Happy riding!",
+    speed: 75,
+    loop: false
+  }).go();
+
+
+
